1a327a482cec2d857817a46b2a33f68ad3e4f9b4
Gary Bake <garybake@gmail.com>
2019-01-20 23:12:11 +0000

    Finished Board()

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -109,13 +109,73 @@ class Board():
             if not self.is_on_grid(neighbour):
                 continue
             neighbour_string = self._grid.get(neighbour)
+
             if neighbour_string is None:
+                # if the neighbour is empty add it to the liberties
                 liberties.append(neighbour)
             elif neighbour_string.color == player:
+                # if the neighbour is ours add it to our pot
                 if neighbour_string not in adjacent_same_color:
                     adjacent_same_color.append(neighbour_string)
             else:
+                # if the neighbour is not ours add it to the other pot
                 if neighbour_string not in adjacent_opposite_color:
                     adjacent_opposite_color.append(neighbour_string)
         new_string = GoString(player, [point], liberties)
 
+        # Merge any adjacent strings of the same color
+        for same_color_string in adjacent_same_color:
+            new_string = new_string.merged_with(same_color_string)
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+
+        # Reduce liberties of any adjacent strings of the opposite color
+        for other_color_string in adjacent_opposite_color:
+            other_color_string.remove_liberty(point)
+        # Remove any opposite-color strings that now have zero liberties
+        for other_color_string in adjacent_opposite_color:
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
+
+    def is_on_grid(self, point):
+        """
+        Returns if the point is within the bounds of the grid
+        """
+        return 1 <= point.row <= self.num_rows and \
+            1 <= point.col <= self.num_cols
+
+    def get(self, point):
+        """
+        Returns content of point on the board
+        Player if stone is on that point
+        else None
+        """
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.color
+
+    def get_go_string(self, point):
+        """
+        Returns entire string of stones at a point
+        GoString if stone is on that point
+        else None
+        """
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string
+
+    def remove_string(self, string):
+        """
+        Removes a string of stones
+        """
+        for point in string.stones:
+            for neighbour in point.neighbours():
+                # Removing a string can create liberties for other strings
+                neighbour_string = self._grid.get(neighbour)
+                if neighbour_string is None:
+                    continue
+                if neighbour_string is not string:
+                    neighbour_string.add_liberty(point)
+            self._grid[point] = None
